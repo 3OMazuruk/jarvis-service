f01336927c3f9e38c2d141374ed6e9e062b999ca
Mazuruk-O <mbeta75@gmail.com>
2020-03-12 13:44:11 +0200

    mock test for ci

---
+++ src/test/java/ua/com/jarvis/AuthServiceApplicationTest.java
@@ -0,0 +1,14 @@
+package ua.com.jarvis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AuthServiceApplicationTest {
+
+    @Test
+    public void test(){
+
+    }
+
+}
